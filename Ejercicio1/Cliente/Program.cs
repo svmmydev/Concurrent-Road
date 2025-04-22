33142e204cc145e5e1e326c87ee16162e13a6809
Sammy <svmmydev@gmail.com>
2025-04-22 03:07:14 +0200

    Initial commit (project structure set)

---
+++ Ejercicio1/Cliente/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Threading;
+using NetworkStreamNS;
+using CarreteraClass;
+using VehiculoClass;
+
+namespace Client
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
---
+++ Ejercicio1/Servidor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Threading;
+using NetworkStreamNS;
+using CarreteraClass;
+using VehiculoClass;
+
+namespace Servidor
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {            
+
+
+        }
+    }
+}
+
